0ad20a74296c3c47bbba4e29c4d81ba356ed711f
NgQuang05 <quang111420@gmail.com>
2025-06-26 15:31:22 +0700

    commit function

--- Function/Bai21.js
+++ Function/Bai21.js
@@ -1,2 +1,19 @@
 //Viết một hàm JavaScript để lấy tất cả các tập hợp con có thể có với tổ hợp có độ dài cố định (ví dụ: 2) trong một mảng.
-function b21(arr, k) {}
+function bai21(arr, k) {
+  const result = [];
+  function backtrack(start, combination) {
+    if (combination.length === k) {
+      result.push([...combination]);
+      return;
+    }
+    for (let i = start; i < arr.length; i++) {
+      combination.push(arr[i]);
+      backtrack(i + 1, combination);
+      combination.pop();
+    }
+  }
+  backtrack(0, []);
+  return result;
+}
+const data = ["a", "b", "c", "d"];
+console.log(bai21(data, 2));
--- Function/Bai26.js
+++ Function/Bai26.js
@@ -1,2 +1,23 @@
 //Viết hàm JavaScript để tìm chuỗi con dài nhất trong một chuỗi cho trước mà không có các ký tự lặp lại.
-function b26(str) {}
+function b26(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    let seen = new Set();
+    let substr = "";
+    for (let j = i; j < str.length; j++) {
+      if (seen.has(str[j])) {
+        break; // gặp trùng thì dừng
+      }
+      seen.add(str[j]);
+      substr += str[j];
+    }
+    if (substr.length > result.length) {
+      result = substr;
+    }
+  }
+
+  return result;
+}
+console.log(b26("abcabcbb"));
+console.log(b26("bbbbb"));
+console.log(b26("pwwkew"));
